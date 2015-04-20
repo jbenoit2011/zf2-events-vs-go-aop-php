46f355d15eb1fc29a0c036134e6f301b7605cd66
Julien BENOIT <jbenoit.jb@gmail.com>
2015-04-20 22:47:11 +0200

    Add aspect

---
+++ aspect/DemoAspect.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Go! OOP&AOP PHP framework
+ *
+ * @copyright     Copyright 2012, Lissachenko Alexander <lisachenko.it@gmail.com>
+ * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
+ */
+
+namespace Aspect;
+
+use Go\Aop\Aspect;
+use Go\Aop\Intercept\FieldAccess;
+use Go\Aop\Intercept\MethodInvocation;
+use Go\Lang\Annotation\After;
+use Go\Lang\Annotation\AfterThrowing;
+use Go\Lang\Annotation\Before;
+use Go\Lang\Annotation\Around;
+use Go\Lang\Annotation\Pointcut;
+
+/**
+ * Debug aspect
+ */
+class DemoAspect implements Aspect
+{
+
+    /**
+     * Method that should be called after real method
+     *
+     * @param MethodInvocation $invocation Invocation
+     * @After("execution(public IndexController->*(*))")
+     */
+    public function afterMethodExecution(MethodInvocation $invocation)
+    {
+        echo 'foo';
+    }
+}
